5c59f1372eb61a7334e52c1f225f2c753baba67f
Marc Coupland <marc@coupland.xyz>
2022-09-04 18:13:33 +0100

    correctly load assets

---
+++ docs/_app/immutable/chunks/1-014accba.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/_error.svelte-9c04e024.js";import"./index-3e215a55.js";import"./singletons-df843fc6.js";export{r as component};
--- docs/_app/immutable/chunks/1-19691c72.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/pages/_error.svelte-3c958236.js";import"./index-3e215a55.js";import"./singletons-263882ae.js";export{r as component};

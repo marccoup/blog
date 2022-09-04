e162b4840b0fd2bc030505ee4254eb3fbcab82a5
Marc Coupland <marc@coupland.xyz>
2022-09-04 18:40:46 +0100

    configure github pages to not use jekyll

--- docs/_app/immutable/chunks/1-014accba.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/pages/_error.svelte-9c04e024.js";import"./index-3e215a55.js";import"./singletons-df843fc6.js";export{r as component};
---
+++ docs/_app/immutable/chunks/1-f7f29113.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/_error.svelte-4cfd6885.js";import"./index-3e215a55.js";import"./singletons-72747ea6.js";export{r as component};

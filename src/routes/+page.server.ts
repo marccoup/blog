5605196d007cc14e3e0f1851c3f6b36d8226d387
Marc Coupland <marc@coupland.xyz>
2022-09-04 17:15:58 +0100

    wip

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,31 +4,41 @@ import Path from "path"
 import yamlFrontMatter from "yaml-front-matter"
 
 export function load({ params }: PageLoad) {
-    const contentDir = fs.readdirSync("static/content/");
+    // const contentDir = fs.readdirSync("content/");
+
+    const example = fs.readdirSync("./");
 
     let posts = [];
 
-    for (let fileName of contentDir) {
-        const filePath = "static/content/" + fileName
-        const file = Path.parse(fileName);
+    // for (let fileName of contentDir) {
+    //     const filePath = "content/" + fileName
+    //     const file = Path.parse(fileName);
 
-        if (file.ext === ".md") {
-            const md = fs.readFileSync(filePath).toString();
-            const front = yamlFrontMatter.loadFront(md);
+    //     if (file.ext === ".md") {
+    //         const md = fs.readFileSync(filePath).toString();
+    //         const front = yamlFrontMatter.loadFront(md);
 
-            if (front.exclude_from_blog !== true) {
-                posts.push({
-                    title: front.title,
-                    date: new Date(Date.parse(front.date)),
-                    url: file.name
-                });
-            }
-        }
+    //         if (front.exclude_from_blog !== true) {
+    //             posts.push({
+    //                 title: front.title,
+    //                 date: new Date(Date.parse(front.date)),
+    //                 url: file.name
+    //             });
+    //         }
+    //     }
+    // }
+
+    for (let fileName of example) {
+        posts.push({
+            title: fileName,
+            date: new Date(),
+            url: '/'
+        })
     }
 
-    posts.sort(function (a, b) {
-        return b.date - a.date
-    })
+    // posts.sort(function (a, b) {
+    //     return b.date - a.date
+    // })
 
     return {
         posts: posts

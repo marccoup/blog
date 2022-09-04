28d8a1e24b63c17b8b1eecb31a68a8ad4a8269a0
Marc Coupland <marc@coupland.xyz>
2022-09-04 20:07:11 +0100

    build

---
+++ docs/_app/immutable/chunks/1-13e3f493.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/_error.svelte-f2e2a6e1.js";import"./index-3e215a55.js";import"./singletons-0c026951.js";export{r as component};
--- docs/_app/immutable/chunks/1-73cc43cc.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/pages/_error.svelte-8900be66.js";import"./index-3e215a55.js";import"./singletons-a5277ca0.js";export{r as component};
